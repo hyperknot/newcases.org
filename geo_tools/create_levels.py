47ec19084e1befc0d6b86a4aa9ec609921cd98fe
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-14 19:53:57 +0100

    levels work

--- geo_tools/create_levels.py
+++ geo_tools/create_levels.py
@@ -17,6 +17,7 @@ states = read_json(geo_tools_dir / 'geojson' / 'states.geojson')['features']
 print(f'{len(states)} states')
 
 levels = dict()
+iso_seen = set()
 
 for feature in subunits:
     prop = feature['properties']
@@ -27,34 +28,38 @@ for feature in subunits:
     country_name = prop['admin']
     country_iso = prop['adm0_a3']
 
-    levels.setdefault(country_name, dict())
-
     unit_name = prop['geounit']
     unit_iso = prop['gu_a3']
 
     subunit_name = prop['subunit']
     subunit_iso = prop['su_a3']
 
-    levels[country_name].setdefault(unit_name, dict())
-    levels[country_name][unit_name].setdefault(subunit_name, dict())
+    levels.setdefault(country_name, {'iso': country_iso, 'l1': {}})
 
-# clean up sole units
-for country, units in levels.items():
-    for unit, subunits in units.items():
-        if len(subunits) == 1:
-            levels[country][unit] = {}
+    l1 = levels[country_name]['l1']
+    l1.setdefault(unit_name, {'iso': unit_iso, 'l2': {}})
 
-# clean up sole subunits
-for country, units in levels.items():
-    if len(units) == 1:
-        levels[country] = {}
+    l2 = l1[unit_name]['l2']
+    l2.setdefault(subunit_name, {'iso': subunit_iso})
 
-    # elif 'iso_3166_2' in prop:
-    #     state_name = prop['name']
-    #     state_iso = prop['iso_3166_2']
-    #
-    #     levels[country_name].setdefault(state_name, dict())
-    #     levels[country_name][state_name].setdefault('-', dict())
+# # clean up sole units
+# for country, units in levels.items():
+#     for unit, subunits in units.items():
+#         if len(subunits) == 1:
+#             levels[country][unit] = {}
+#
+# # clean up sole subunits
+# for country, units in levels.items():
+#     print(units)
+# if len(units) == 1:
+#     levels[country] = {}
+
+# elif 'iso_3166_2' in prop:
+#     state_name = prop['name']
+#     state_iso = prop['iso_3166_2']
+#
+#     levels[country_name].setdefault(state_name, dict())
+#     levels[country_name][state_name].setdefault('-', dict())
 
 
 write_json(pathlib.Path('levels.json'), levels, indent=2)
