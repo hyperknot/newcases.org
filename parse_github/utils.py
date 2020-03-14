ab57fe4d56fe7378716ad0bb9174b51d5e211e72
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-14 03:46:13 +0100

    requirements config

--- parse_github/utils.py
+++ parse_github/utils.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
-
 import requests
+
 
 def get_csv():
     url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
 
-    requests.get
+    r = requests.get(url)
+    r.raise_for_status()
+
+    return r.text
