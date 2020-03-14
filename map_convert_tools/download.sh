021d07f8d52c4e7d1d6ae4de20cc9059a9384340
Zsolt Ero <zsolt.ero@gmail.com>
2020-03-14 16:40:43 +0100

    added subunits

--- map_convert_tools/download.sh
+++ map_convert_tools/download.sh
@@ -5,8 +5,10 @@ mkdir shp
 
 wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
 wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_map_units.zip
+wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_map_subunits.zip
 
 unzip ne_50m_admin_0_countries.zip -d shp/countries
-unzip ne_50m_admin_0_map_units.zip -d shp/mapunits
+unzip ne_50m_admin_0_map_units.zip -d shp/map_units
+unzip ne_50m_admin_0_map_subunits.zip -d shp/map_subunits
 
 rm *.zip
--- map_convert_tools/shp2geo.sh
+++ map_convert_tools/shp2geo.sh
@@ -6,4 +6,5 @@ mkdir geojson
 yarn
 
 yarn shp2json shp/countries/ne_50m_admin_0_countries.shp -o geojson/countries.geojson
-yarn shp2json shp/mapunits/ne_50m_admin_0_map_units.shp -o geojson/mapunits.geojson
+yarn shp2json shp/map_units/ne_50m_admin_0_map_units.shp -o geojson/map_units.geojson
+yarn shp2json shp/map_subunits/ne_50m_admin_0_map_subunits.shp -o geojson/map_subunits.geojson
