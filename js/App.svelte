277b4e8896a8446a6fb3bb53ef9a50f0780b691d
Zsolt Ero <zsolt.ero@gmail.com>
2020-05-04 04:30:04 +0200

    fetch json

--- js/App.svelte
+++ js/App.svelte
@@ -1,10 +1,12 @@
 <script>
+  import { onMount } from 'svelte'
   import router from 'page'
   import ChartBoard from './ChartBoard.svelte'
   import Country from './Country.svelte'
 
   let page
   let params
+  let timeseries
 
   router('/', () => (page = ChartBoard))
   router(
@@ -15,8 +17,16 @@
     },
     () => (page = Country)
   )
-
   router.start()
+
+  onMount(async () => {
+    try {
+      const res = await fetch('/export/timeseries.json')
+      timeseries = await res.json()
+    } catch (e) {
+      console.error(`error loading timeseries.json: ${e}`)
+    }
+  })
 </script>
 
 <svelte:component this={page} {params} />
