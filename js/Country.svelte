2815ca01af45da20c598daa59d108338554fc5bb
Zsolt Ero <zsolt.ero@gmail.com>
2020-05-04 03:48:29 +0200

    prettier

--- js/Country.svelte
+++ js/Country.svelte
@@ -1,20 +1,20 @@
 <script>
-    import Chart from './Chart.svelte'
-    import data from '../data/country_data.json';
+  import Chart from './Chart.svelte'
+  import data from '../data/country_data.json'
 
-    export let params;
+  export let params
 
-    let country = params.id;
+  let country = params.id
 </script>
 
 <main>
-    <h1>{country}</h1>
-    <Chart data={data[country]} />
+  <h1>{country}</h1>
+  <Chart data={data[country]} />
 </main>
 
 <style>
-    main {
-        width: 90%;
-        margin: 0 auto;
-    }
+  main {
+    width: 90%;
+    margin: 0 auto;
+  }
 </style>
